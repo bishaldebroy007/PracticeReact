2f3d7c1f7575816ce30a3bc365036c4b53ef59f1
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-02-03 15:12:19 +0600

    App to ListGroup transfered

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,9 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  const cityList = ["Dhaka", "Sylhet", "CTG"];
-
   return (
     <>
-      <h1>List</h1>
-      <ul className="list-group">
-        {/* Dynamic Way: */}
-        {cityList.map((cityList) => (
-          <li>{cityList}</li>
-        ))}
-        {/* Old Ways: */}
-        {/* <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li> */}
-      </ul>
+      <ListGroup />
     </>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,33 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  return <h1>List Group</h1>;
+  const cityList = ["Dhaka", "Sylhet", "Chittagong", "Rangpur"];
+  // cityList = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {cityList.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {/* Dynamic Way: */}
+        {cityList.map((names, index) => (
+          <li
+            className="list-group-item"
+            key={names}
+            onClick={() => console.log(names, index)}
+          >
+            {names}
+          </li>
+        ))}
+        {/* Old Ways: */}
+        {/* <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li> */}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
